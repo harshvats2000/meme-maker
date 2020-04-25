d7ce84fceb52ef40c04aceea2aafcf410b615a62
harshvats2000 <vatsvatsharsh2000@gmail.com>
2020-04-25 23:07:00 +0530

    Updates

---
+++ precache-manifest.6c9cf20d48ff689d7f8570e8c339ac59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d217a554b6d13254924a3e9377fd52f",
+    "url": "/bestshayari/index.html"
+  },
+  {
+    "revision": "1f280b153d0b9df09065",
+    "url": "/bestshayari/static/css/main.6d8efb46.chunk.css"
+  },
+  {
+    "revision": "316aeb80b5dfd383ab01",
+    "url": "/bestshayari/static/js/2.ac01e46d.chunk.js"
+  },
+  {
+    "revision": "1f280b153d0b9df09065",
+    "url": "/bestshayari/static/js/main.04029b91.chunk.js"
+  },
+  {
+    "revision": "bf2691f990cf7869623c",
+    "url": "/bestshayari/static/js/runtime~main.f0341ecf.js"
+  }
+]);
