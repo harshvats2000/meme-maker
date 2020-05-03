e57075d51caceb6ab1724336f57b33b15f43cde0
harshvats2000 <vatsvatsharsh2000@gmail.com>
2020-05-04 00:35:58 +0530

    Updates

---
+++ precache-manifest.27d8f99917ccacd0496de31c0b7306b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59cbf990e70b517d156cd49de5d0e2c3",
+    "url": "/bestshayari/index.html"
+  },
+  {
+    "revision": "e1b9cf5f66229b862d98",
+    "url": "/bestshayari/static/css/2.3a2b78f5.chunk.css"
+  },
+  {
+    "revision": "a1ae6025816186393e48",
+    "url": "/bestshayari/static/css/main.c4a380a7.chunk.css"
+  },
+  {
+    "revision": "e1b9cf5f66229b862d98",
+    "url": "/bestshayari/static/js/2.8595f7be.chunk.js"
+  },
+  {
+    "revision": "a1ae6025816186393e48",
+    "url": "/bestshayari/static/js/main.6a3ec43e.chunk.js"
+  },
+  {
+    "revision": "bf2691f990cf7869623c",
+    "url": "/bestshayari/static/js/runtime~main.f0341ecf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/bestshayari/precache-manifest.20326c90092f662bbf5c9a44773e96e1.js"
+  "/bestshayari/precache-manifest.27d8f99917ccacd0496de31c0b7306b0.js"
 );
 
 self.addEventListener('message', (event) => {
