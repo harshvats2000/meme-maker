30f719ceda72f0011f935858e3cffecffd08f677
harshvats2000 <vatsvatsharsh2000@gmail.com>
2020-05-03 20:03:54 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/bestshayari/precache-manifest.65b26eb0bd1b5e05336c994c3d720a92.js"
+  "/bestshayari/precache-manifest.20326c90092f662bbf5c9a44773e96e1.js"
 );
 
 self.addEventListener('message', (event) => {
