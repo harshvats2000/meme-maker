e23920fcf256a8c7b8e8755e8cb90d516af3be98
harshvats2000 <vatsvatsharsh2000@gmail.com>
2020-05-17 15:41:39 +0530

    changed hash router

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import firebase from 'firebase'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 var firebaseConfig = {
     apiKey: "AIzaSyCEMfOB5_j1hIdpxVXOduBuUMTwj5txVZo",
@@ -20,9 +20,9 @@ firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>, 
+    </HashRouter>, 
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
